8a45d103e04da3793318cf77e17657aba082a986
sgsk88 <sgsk88@nate.com>
2022-01-18 17:38:56 +0900

    git ssongsik commit

---
+++ src/main/java/com/example/demo/src/project/ProjectController.java
@@ -0,0 +1,32 @@
+package com.example.demo.src.project;
+
+import com.example.demo.config.BaseResponse;
+import com.example.demo.utils.JwtService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+@RequestMapping("/project")
+public class ProjectController {
+    final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private final ProjectProvider projectProvider;
+    @Autowired
+    private final ProjectService projectService;
+    @Autowired
+    private final JwtService jwtService;
+
+    public ProjectController(ProjectProvider projectProvider, ProjectService projectService, JwtService jwtService) {
+        this.projectProvider = projectProvider;
+        this.projectService = projectService;
+        this.jwtService = jwtService;
+    }
+
+}
---
+++ src/main/java/com/example/demo/src/project/ProjectDao.java
@@ -0,0 +1,4 @@
+package com.example.demo.src.project;
+
+public class ProjectDao {
+}
