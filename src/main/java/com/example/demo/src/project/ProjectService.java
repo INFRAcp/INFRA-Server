8aecd8781c0be85aa742a4a3931b50427eb59ee0
CodeDiary18 <codediary18@gmail.com>
2022-02-10 23:28:16 +0900

    ProjectService userIdJwt 함수 수정

--- src/main/java/com/example/demo/src/project/ProjectService.java
+++ src/main/java/com/example/demo/src/project/ProjectService.java
@@ -1,7 +1,6 @@
 package com.example.demo.src.project;
 
 import com.example.demo.config.BaseException;
-import com.example.demo.config.BaseResponse;
 import com.example.demo.src.project.model.*;
 import com.example.demo.utils.JwtService;
 import org.slf4j.Logger;
@@ -125,6 +124,7 @@ public class ProjectService {
 
     /**
      * 프로젝트 찜 등록
+     *
      * @param postLikeRegisterReq
      * @return 등록 완료된 메세지
      * @author 윤성식
@@ -141,6 +141,7 @@ public class ProjectService {
 
     /**
      * 프로젝트 찜 삭제
+     *
      * @param postLikeRegisterReq
      * @return 찜 삭제된 메세지
      * @author 윤성식
@@ -384,16 +385,16 @@ public class ProjectService {
 
     /**
      * 유저 JWT 유효성 검사
+     *
      * @param userId
      * @param userIdByJwt
      * @return BaseResponse
-     * @author 한규범
+     * @author 한규범, 강윤희
      */
-    public BaseResponse<Object> userIdJwt(String userId, String userIdByJwt){
+    public void userIdJwt(String userId, String userIdByJwt) throws BaseException {
         if (!userId.equals(userIdByJwt)) {
-            return new BaseResponse<>(INVALID_USER_JWT);
+            throw new BaseException(INVALID_USER_JWT);
         }
-        return null;
     }
 
 
