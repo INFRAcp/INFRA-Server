124d78f0ab72aa5e163fde4d1b94e2af9ed70eb5
shinhn <sun998496@gmail.com>
2022-02-15 23:42:19 +0900

    s3 upload

---
+++ src/main/java/com/example/demo/src/s3/S3Controller.java
@@ -0,0 +1,22 @@
+package com.example.demo.src.s3;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+@RestController
+public class S3Controller {
+
+    private final S3Uploader s3Uploader;
+
+    @PostMapping("/images")
+    public String upload(@RequestParam("images") MultipartFile multipartFile) throws IOException {
+        s3Uploader.upload(multipartFile, "static");
+        return "s3에 업로드 완료.";
+    }
+}
