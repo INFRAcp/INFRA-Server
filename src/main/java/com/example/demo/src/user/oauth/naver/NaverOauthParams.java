684cb715d2bd17433218036877db26a33926cb75
ye-Oni <yewon981207@gmail.com>
2022-02-20 23:39:25 +0900

    naver 로그인 하다만 그 어디쯤....

---
+++ src/main/java/com/example/demo/src/user/oauth/naver/NaverOauthParams.java
@@ -0,0 +1,7 @@
+package com.example.demo.src.user.oauth.naver;
+
+public class NaverOauthParams {
+    public String getAccess_token() {
+        return "accessToken : " + getAccess_token();
+    }
+}
